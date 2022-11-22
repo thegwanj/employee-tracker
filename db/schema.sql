3f74c260b83ba4cf56ea0bc776cae0dbf3cf4aa3
Gwanju <gwanjuchung@yahoo.com>
2022-11-21 21:32:13 -0800

    Filled in the code to create the tables

--- db/schema.sql
+++ db/schema.sql
@@ -7,12 +7,32 @@ USE company_db;
 -- Create departments, roles, and employees tables
 CREATE TABLE departments (
     -- name
+    id INT AUTO_INCREMENT,
+    name VARCHAR(30),
+    PRIMARY KEY(id),
+    ON DELETE CASCADE
 );
 
 CREATE TABLE roles (
     -- title, salary, department_id
+    id INT AUTO_INCREMENT NOT NULL,
+    title VARCHAR(30),
+    salary DECIMAL,
+    department_id INT,
+    PRIMARY KEY(id),
+    FOREIGN KEY(department_id)
+    REFERENCES departments(id),
+    ON DELETE SET NULL
 );
 
 CREATE TABLE employees (
     -- first_name, last_name, role_id, manager_id
+    id INT AUTO_INCREMENT NOT NULL,
+    first_name VARCHAR(30),
+    last_name VARCHAR(30),
+    role_id INT,
+    manager_id INT,
+    PRIMARY KEY(id),
+    FOREIGN KEY(role_id)
+    REFERENCES roles(id),
 );
