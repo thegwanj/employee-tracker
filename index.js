e3320f78ea197958b995226abd1d4b652fef1243
Gwanju <gwanjuchung@yahoo.com>
2022-11-21 20:01:29 -0800

    Added in additional pseudocode

--- index.js
+++ index.js
@@ -6,3 +6,20 @@
 
 // View all employees
 // SELECT * FROM employees
+
+// Create new departments
+// INSERT INTO departments (column names)
+// VALUES (values)
+
+// Create new roles
+// INSERT INTO roles (column names)
+// VALUES (values)
+
+// Create new employees
+// INSERT INTO employees (column names)
+// VALUES (values)
+
+// Update employee
+// UPDATE employees
+// SET column1 = value, column2 = value
+// WHERE (conditional)
