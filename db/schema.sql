6312bdf9e33addc60bc3bede3fccb23bb94777c7
Gwanju <gwanjuchung@yahoo.com>
2022-11-21 20:10:46 -0800

    Added in guiding comments

--- db/schema.sql
+++ db/schema.sql
@@ -6,13 +6,13 @@ USE company_db;
 
 -- Create departments, roles, and employees tables
 CREATE TABLE departments (
-
+    -- name
 );
 
 CREATE TABLE roles (
-
+    -- title, salary, department_id
 );
 
 CREATE TABLE employees (
-
+    -- first_name, last_name, role_id, manager_id
 );
