357e0d7f4f5b98c3b1ada97064535cf28af6faca
Gwanju <gwanjuchung@yahoo.com>
2022-11-21 19:56:57 -0800

    Added in code to create our db and tables

--- db/schema.sql
+++ db/schema.sql
@@ -1,4 +1,18 @@
 -- Create and use our database
+DROP DATABASE IF EXISTS company_db;
+CREATE DATABASE company_db;
 
+USE company_db;
 
 -- Create departments, roles, and employees tables
+CREATE TABLE departments (
+
+);
+
+CREATE TABLE roles (
+
+);
+
+CREATE TABLE employees (
+
+);
